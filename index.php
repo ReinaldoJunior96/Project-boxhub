a2a77199528424699f9de7de4a28a0a48814805c
Reinaldo Júnior <33222779+ReinaldoJunior96@users.noreply.github.com>
2020-09-11 17:03:17 -0300

    altualiznado index

--- index.php
+++ index.php
@@ -8,30 +8,28 @@ $s = new ConfigCRUD();
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs4-4.1.1/dt-1.10.18/datatables.min.css"/>
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs4-4.1.1/dt-1.10.18/datatables.min.css" />
     <link rel="stylesheet" href="css/style.min.css">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
-          integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
     <!-- Meu CSS -->
     <title>Box Hub</title>
     <!-- <link rel="icon" class="rounded" href="images/icon-box.png" type="image/x-icon" /> -->
 </head>
+
 <body>
-<div class="container-fluid">
-    <div class="row">
-        <?php include_once "widget/menu.php" ?>
-        <div class="col-9">
-            <div class="">
-                <nav class="navbar navbar-expand-lg navbar-light bg-nav">
+    <div class="container-fluid">
+        <div class="row">
+            <?php include_once "widget/menu.php" ?>
+            <div class="col-9">
+                <!--<nav class="navbar navbar-expand-lg navbar-light bg-nav">
                     <a class="navbar-brand text-white roboto-condensed" href="#"><i class="fas fa-box-open"></i>
                     </a>
-                    <button class="navbar-toggler" type="button" data-toggle="collapse"
-                            data-target="#conteudoNavbarSuportado" aria-controls="conteudoNavbarSuportado"
-                            aria-expanded="false" aria-label="Alterna navegação">
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#conteudoNavbarSuportado" aria-controls="conteudoNavbarSuportado" aria-expanded="false" aria-label="Alterna navegação">
                         <span class="navbar-toggler-icon"></span>
                     </button>
 
@@ -40,23 +38,22 @@ $s = new ConfigCRUD();
 
                         </ul>
                         <div class="form-inline my-2 my-lg-0">
-                            <!--<a href="#" class="badge badge-secondary"><i class="fas fa-bell text-white"></i> <span
-                                        class="badge text-white">5</span></a>-->
+                            <a href="#" class="badge badge-secondary"><i class="fas fa-bell text-white"></i> <span
+                                        class="badge text-white">5</span></a>
                         </div>
                     </div>
-                </nav>
-                <div class="text-center mt-5">
-                    <img src="images/index-image.png" width="600" class="img-fluid" alt="Imagem responsiva">
-                    <br><br><br><br><br><br><br><br><br>
+                </nav>-->
+                <div class="text-center mt-4">
+                    <img src="images/box3.png" width="600" class="img-fluid" alt="Imagem responsiva">
                 </div>
             </div>
         </div>
     </div>
-</div>
-<!-- JavaScript (Opcional) -->
-<!-- jQuery primeiro, depois Popper.js, depois Bootstrap JS -->
-<script src="node_modules/jquery/dist/jquery.slim.min.js"></script>
-<script src="node_modules/popper.js/dist/umd/popper.min.js"></script>
-<script src="node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
+    <!-- JavaScript (Opcional) -->
+    <!-- jQuery primeiro, depois Popper.js, depois Bootstrap JS -->
+    <script src="node_modules/jquery/dist/jquery.slim.min.js"></script>
+    <script src="node_modules/popper.js/dist/umd/popper.min.js"></script>
+    <script src="node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
 </body>
+
 </html>
