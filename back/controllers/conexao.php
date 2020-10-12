976ba3a8fcc20dadda629609b051812493446ec0
Reinaldo Júnior <33222779+ReinaldoJunior96@users.noreply.github.com>
2020-10-12 20:59:17 -0300

    alteração bd

--- back/controllers/conexao.php
+++ back/controllers/conexao.php
@@ -13,10 +13,10 @@ class PDOconectar
     public static function conectar()
     {
         try {
-            //return new PDO("mysql:host=localhost;dbname=boxhub;charset=utf8","root","",
-           //    array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
-            return new PDO("mysql:host=mysql.uemapet.com;dbname=uemapet;charset=utf8", "uemapet", "Reinaldo@123",
-                array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+            return new PDO("mysql:host=localhost;dbname=boxhub;charset=utf8","root","",
+               array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+            //return new PDO("mysql:host=mysql.uemapet.com;dbname=uemapet;charset=utf8", "uemapet", "Reinaldo@123",
+            //    array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
         } catch (PDOException $ex) {
             echo "Erro: " . $ex->getMessage();
         }
